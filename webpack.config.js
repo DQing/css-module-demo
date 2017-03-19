7222751fc7fceab1071acd270b22ec33018cdbe7
DQing <15829258737@163.com>
2017-03-19 17:28:37 +0800

    make frame

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,24 @@
+var path = require('path');
 module.exports = {
-    entry: "./entry.js",
+    entry: './public/src/entry.js',
     output: {
-        path: __dirname,
-        filename: "bundle.js"
+        path: path.join(__dirname, 'public/dist'),
+        filename: 'bundle.js'
     },
     module: {
         loaders: [
-            { test: /\.css$/, loader: "style-loader!css-loader" }
+            {test: /\.css$/, loader: "style-loader!css-loader"},
+            {test: /\.png$/, loader: "file-loader"},
+            {test: /\.jpg$/, loader: "file-loader"},
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    cacheDirectory: true,
+                    presets: ['react', 'es2015']
+                }
+            }
         ]
     }
 };
