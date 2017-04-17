f4125275dcb43cbec6c365cdde7e2fb519b9d123
DQing <15829258737@163.com>
2017-04-17 16:00:45 +0800

    modify

--- public/src/component/LoginAndRegister/login.js
+++ public/src/component/LoginAndRegister/login.js
@@ -4,12 +4,11 @@ import style from "../../../assest/css/login.css"
 
 class Login extends Component {
 
-      render() {
+
+    render() {
         return <div>
             <form action="">
                 用户名：<input id="userName" type="text" placeholder="输入姓名" className={style.title}/>
-                密码：<input id="passWord" type="text" placeholder="输入密码"/>
-                <input type="submit" value='提交'/>
             </form>
         </div>
     }
--- public/src/component/LoginAndRegister/register.js
+++ public/src/component/LoginAndRegister/register.js
@@ -2,7 +2,7 @@ import React, {Component} from "react";
 import {render} from "react-dom";
 import style from '../../../assest/css/resigner.css';
 
-class Login extends Component {
+class Resigner extends Component {
 
 
     render() {
@@ -10,23 +10,8 @@ class Login extends Component {
             <div>
                 <input id="userName" type="text" placeholder="输入姓名" className={style.title}/>
             </div>
-            <div>
-                <input id="passWord" type="text" placeholder="输入密码"/>
-            </div>
-            <div>
-                <input id="repassWord" type="text" placeholder="再次输入密码"/>
-            </div>
-            <div>
-                <input type="text" id="telephone" placeholder="输入电话号码"/>
-            </div>
-            <div>
-                性别：男<input type="radio" id="sex" name="sex"/>
-                女<input type="radio" name="sex"/>
-            </div>
-            <div>
-                <input type="submit" value='注册'/>
-            </div>
+            <input type="submit" value='注册'/>
         </div>
     }
 }
-export default Login;
+export default Resigner;
